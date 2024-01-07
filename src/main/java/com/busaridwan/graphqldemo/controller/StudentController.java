88291c2ce09450a40e7f2b0bf3b7cac0ff364455
Muhammed Busari Ridwan <busaridwan@gmail.com>
2024-01-07 23:01:41 +0100

    added schema

--- src/main/java/com/busaridwan/graphqldemo/controller/StudentController.java
+++ src/main/java/com/busaridwan/graphqldemo/controller/StudentController.java
@@ -1,7 +1,38 @@
 package com.busaridwan.graphqldemo.controller;
 
+import com.busaridwan.graphqldemo.model.Level;
+import com.busaridwan.graphqldemo.model.Student;
+import com.busaridwan.graphqldemo.service.StudentService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StudentController {
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @QueryMapping
+    public List<Student> findAll(){
+        return service.findAll();
+    }
+    @QueryMapping
+    public Optional<Student> findById(@Argument Integer id){
+        return service.findById(id);
+    }
+    @MutationMapping
+    public Student create(@Argument String name, @Argument Level level){
+        return service.create(name, level);
+    }
+    @MutationMapping
+    public Student update(@Argument Integer id, @Argument String name, @Argument Level level){
+        return service.update(id,name,level);
+    }
 }
