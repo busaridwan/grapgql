5c7f2ace5b04b22df4222dfdb469d498720b6eb9
Muhammed Busari Ridwan <busaridwan@gmail.com>
2024-01-07 22:18:36 +0100

    adding service

---
+++ src/main/java/com/busaridwan/graphqldemo/controller/StudentController.java
@@ -0,0 +1,7 @@
+package com.busaridwan.graphqldemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+}
---
+++ src/main/java/com/busaridwan/graphqldemo/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.busaridwan.graphqldemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+}
